8ee632fdfee98b4ed82481a02d0c11cdbf2e2c56
ronyvilleda <rony.lopezvilleda@telus.com>
2023-03-17 09:53:29 -0400

    feat: add result of the session

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -4,7 +4,10 @@ import Home from "./index";
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />);
-    const heading = screen.getByRole("heading", { name: /Calculator/ });
-    expect(heading).toBeInTheDocument();
+    const heading = screen.getByRole("heading", { name: "Calculator" });
+    const button1 = screen.getByRole("button", { name: "1" });
+    const input = screen.getByTestId("display");
+    expect(heading).toBeInTheDocument;
+    expect(button1).toBeInTheDocument;
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <h1>Calculator</h1>  
+        <h1>Calculator</h1>
+        <input type="text" data-testid="display"></input>
+        <button>1</button>
       </main>
     </>
-  )
+  );
 }
